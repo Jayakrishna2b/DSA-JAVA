41a30c99fb5d89d2154b5b26306a3bf718574aad
Jayakrishna2b <123311468+Jayakrishna2b@users.noreply.github.com>
2023-07-20 00:04:39 +0530

    Create Input.java

---
+++ TOPICS/Input.java
@@ -0,0 +1,11 @@
+package com.krishna;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+              String name = input.next();
+        System.out.println(name);
+         }
+}
