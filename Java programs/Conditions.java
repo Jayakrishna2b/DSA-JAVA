61d3cf4b6862d33f5b6a18af7a32e1ba6a88cc14
Jayakrishna2b <123311468+Jayakrishna2b@users.noreply.github.com>
2023-07-20 22:00:07 +0530

    Create Conditions.java

---
+++ Java programs/Conditions.java
@@ -0,0 +1,15 @@
+package com.krishna;
+
+public class Conditions {
+    public static void main(String[] args) {
+        int revenue = 8400000;
+        if(revenue > 7000000) {
+            revenue = revenue + 70000;
+
+        } else {
+            revenue = revenue + 1;
+        }
+        System.out.println(revenue);
+
+    }
+}
