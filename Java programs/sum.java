20a3a68df3408752bc31a4b4d3ba4bcdc0aa22ef
Jayakrishna2b <123311468+Jayakrishna2b@users.noreply.github.com>
2023-07-29 23:15:52 +0530

    Update sum.java

--- Java programs/sum.java
+++ Java programs/sum.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class sum {
     public static void main(String[] args) {
+        sum ();
+    }
+    static void sum() {
         Scanner input = new Scanner(System.in);
-        float num1 = input.nextFloat();
-        float num2 = input.nextFloat();
-
-        float sum = num1 +num2;
-        System.out.println("sum = " + sum);
+        System.out.print("enter the number 1");
+        int num1 = input.nextInt();
+        System.out.print("enter the number 2");
+        int num2 = input.nextInt();
+        int sum =num1+num2;
+        System.out.println("the sum =" + sum);
     }
 }
